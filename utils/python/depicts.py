76bf831d42dadd21618ce5239d8db2cc14563b21
thisisaaronland <devnull@localhost>
2020-02-26 15:25:37 -0800

    wof:depicts for Qantas

--- utils/python/depicts.py
+++ utils/python/depicts.py
@@ -10,13 +10,14 @@ import re
 
 """
 "John F. Kennedy International Airport (JFK)":[ 102534365 ]
-
+"Boeing 747-400": [ 1159289915, 1159293593, 1159289873 ]
+"Qantas Airways": [ 1159285043 ]
 """
 
 if __name__ == "__main__":
 
-    depicts_label = "Boeing 747-400"
-    depicts_id = [ 1159289915, 1159293593, 1159289873 ]
+    depicts_label = "Qantas Airways"
+    depicts_id = [ 1159285043 ]
 
     p = r'.*' + re.escape(depicts_label) + '.*'
     pat = re.compile(p)
